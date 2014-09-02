fd9b2068cc7b82d073985cdb3b4abbdd8bc0b2c0
Deepankar Roy <roy1608@gmail.com>
2014-09-02 23:06:03 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////
+//Deepankar Roy
+//Homework 1
+//Welcome Class Java Program
+
+//define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args)  {
+        
+//print the statement with symbols around it
+       System.out.println("    -----------         ");
+       System.out.println("    | WELCOME |         ");
+       System.out.println("    -----------         ");
+       System.out.println("  ^  ^  ^  ^  ^  ^    ");
+       System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\   ");
+       System.out.println(" |-D--E--R--2--1--7-| ");
+       System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /   ");
+       System.out.println("  v  v  v  v  v  v    ");
+       System.out.println("This summer my friend interned at 'Late Night with Seth Meyers' and i went to go visit her at work and I got to meet Seth Meyers");
+////look up printing special characters
+    }
+    
+}
